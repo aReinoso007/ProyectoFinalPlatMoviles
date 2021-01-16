6b007d1148484f2b1addd1fe2512ded0bb955213
Alex Reinoso <ralexjessiel@gmail.com>
2021-01-15 18:19:47 -0800

    Registro usuario
    creado formularios de registro, servicio de crear usuario, falta validad contrasena

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from '../model/usuario';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
-  constructor() { }
+  
+  usuario: Usuario = new Usuario();
+  contrasena: string;
+  estado: boolean = false;
+  constructor(public router: Router, private route: ActivatedRoute,public  usuarioService: UsuarioService
+    ) { }
 
   ngOnInit() {
+  }
+
+  registrarNuevoUsuario(){
+    this.usuarioService.registrarUsuario(this.usuario);
+    console.log('usuario registrado con exito!');
+  }
+
+  verificarContrasena(pass1: string, pass2: string){
+    pass1 = this.usuario.password.toString();
+    pass2 = this.contrasena.toString();
+    if(pass1 == pass2){
+      this.estado = true;
+      return this.estado;
+    }
+    else{
+      return this.estado;
+    }
+
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { merge, Observable } from 'rxjs';
+import { Usuario } from '../model/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(public afs: AngularFirestore) { }
+
+  registrarUsuario(usuario: Usuario){
+    const refContacto = this.afs.collection("usuario");
+
+    if (usuario.uid == null ){
+      usuario.uid = this.afs.createId();
+    }
+    refContacto.doc(usuario.uid).set(Object.assign({}, usuario), {merge: true})
+  }
+
+  getUsuarios(): Observable<any[]>{
+    return this.afs.collection("usuario").valueChanges();
+  }
 }
+
